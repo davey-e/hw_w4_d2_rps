86126f0f639abd07b58f5f29ccf4f826a9efb166
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-12 22:33:50 +0000

    Updated controller.rb to use new Game class instead of having logic in controller

--- controller.rb
+++ controller.rb
@@ -1,15 +1,9 @@
 require('sinatra')
 require('sinatra/contrib/all') if development?()
 require('pry-byebug')
+require_relative('./models/game.rb')
 
 get('/game/:hand1/:hand2') do
-  return 'Paper wins!' if params['hand1'] == 'rock' && params['hand2'] == 'paper'
-  return 'Rock wins!' if params['hand1'] == 'rock' && params['hand2'] == 'scissors'
-  return 'Scissors wins!' if params['hand1'] == 'paper' && params['hand2'] == 'scissors'
-  return 'Paper wins!' if params['hand1'] == 'paper' && params['hand2'] == 'rock'
-  return 'Rock wins!' if params['hand1'] == 'scissors' && params['hand2'] == 'rock'
-  return 'Scissors wins!' if params['hand1'] == 'scissors' && params['hand2'] == 'paper'
-  return 'Its a draw!' if params['hand1'] == 'rock' && params['hand2'] == 'rock'
-  return 'Its a draw!' if params['hand1'] == 'paper' && params['hand2'] == 'paper'
-  return 'Its a draw!' if params['hand1'] == 'scissors' && params['hand2'] == 'scissors'
+  game = Game.new(params[:hand1], params[:hand2])
+  return game.play_game()
 end
