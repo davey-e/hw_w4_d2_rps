4cbc80d8a13d9c8ff50fddb92abc051c9245f9f1
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-12 23:28:16 +0000

    Added home page with game rules

--- controller.rb
+++ controller.rb
@@ -3,6 +3,10 @@ require('sinatra/contrib/all') if development?()
 require('pry-byebug')
 require_relative('./models/game.rb')
 
+get('/') do
+    erb(:home)
+end
+
 get('/game/:hand1/:hand2') do
   game = Game.new(params[:hand1], params[:hand2])
   @game_result = game.play_game()
